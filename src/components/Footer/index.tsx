2fb027e04f361731dbd60105a8039128c7cbb571
Sadra <84704927+sadra-hub@users.noreply.github.com>
2025-11-13 17:04:41 +0100

    update footer content and localization; replace email and address, add Dutch translations

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -50,42 +50,54 @@ const Footer = ({ t }: { t: TFunction }) => {
       <FooterSection>
         <Container>
           <Row justify="space-between">
+            {/* Contact column */}
             <Col lg={10} md={10} sm={12} xs={12}>
               <Language>{t("Contact")}</Language>
               <Large to="/">{t("Tell us everything")}</Large>
               <Para>
                 {t(`Do you have any question? Feel free to reach out.`)}
               </Para>
-              <a href="mailto:l.qqbadze@gmail.com">
+              <a href="mailto:info@systein.nl">
                 <Chat>{t(`Let's Chat`)}</Chat>
               </a>
             </Col>
+
+            {/* Policy column */}
             <Col lg={8} md={8} sm={12} xs={12}>
               <Title>{t("Policy")}</Title>
-              <Large to="/">{t("Application Security")}</Large>
-              <Large to="/">{t("Software Principles")}</Large>
+              <Large to="/privacy">{t("Application Security")}</Large>
+              <Large to="/privacy">{t("Software Principles")}</Large>
             </Col>
+
+            {/* Support column */}
             <Col lg={6} md={6} sm={12} xs={12}>
               <Empty />
-              <Large to="/">{t("Support Center")}</Large>
-              <Large to="/">{t("Customer Support")}</Large>
+              <Large to="/support">{t("Support Center")}</Large>
+              <Large to="/support">{t("Customer Support")}</Large>
             </Col>
           </Row>
+
           <Row justify="space-between">
+            {/* Address column */}
             <Col lg={10} md={10} sm={12} xs={12}>
               <Empty />
               <Language>{t("Address")}</Language>
-              <Para>Rancho Santa Margarita</Para>
-              <Para>2131 Elk Street</Para>
-              <Para>California</Para>
+              <Para>SYSTEIN Solutions</Para>
+              <Para>De Lismortel 42 366</Para>
+              <Para>5612 AR Eindhoven</Para>
+              <Para>The Netherlands</Para>
             </Col>
+
+            {/* Company / navigation column */}
             <Col lg={8} md={8} sm={12} xs={12}>
               <Title>{t("Company")}</Title>
-              <Large to="/">{t("About")}</Large>
-              <Large to="/">{t("Blog")}</Large>
-              <Large to="/">{t("Press")}</Large>
-              <Large to="/">{t("Careers & Culture")}</Large>
+              <Large to="/about">{t("About")}</Large>
+              <Large to="/product">{t("Product")}</Large>
+              <Large to="/mission">{t("Mission")}</Large>
+              <Large to="/policy">{t("Policy")}</Large>
             </Col>
+
+            {/* Language switch */}
             <Col lg={6} md={6} sm={12} xs={12}>
               <Label htmlFor="select-lang">{t("Language")}</Label>
               <LanguageSwitchContainer>
@@ -97,9 +109,9 @@ const Footer = ({ t }: { t: TFunction }) => {
                     height="30px"
                   />
                 </LanguageSwitch>
-                <LanguageSwitch onClick={() => handleChange("es")}>
+                <LanguageSwitch onClick={() => handleChange("nl")}>
                   <SvgIcon
-                    src="spain.svg"
+                    src="netherlands.svg"
                     aria-label="homepage"
                     width="30px"
                     height="30px"
@@ -110,6 +122,7 @@ const Footer = ({ t }: { t: TFunction }) => {
           </Row>
         </Container>
       </FooterSection>
+
       <Extra>
         <Container border={true}>
           <Row
@@ -127,35 +140,16 @@ const Footer = ({ t }: { t: TFunction }) => {
                 />
               </LogoContainer>
             </NavLink>
+
             <FooterContainer>
               <SocialLink
-                href="https://github.com/Adrinlol/create-react-app-adrinlol"
+                href="https://github.com/SystEinNL"
                 src="github.svg"
               />
               <SocialLink
-                href="https://twitter.com/Adrinlolx"
-                src="twitter.svg"
-              />
-              <SocialLink
-                href="https://www.linkedin.com/in/lasha-kakabadze/"
+                href="https://www.linkedin.com/company/systein"
                 src="linkedin.svg"
               />
-              <SocialLink
-                href="https://medium.com/@lashakakabadze/"
-                src="medium.svg"
-              />
-              <a
-                href="https://ko-fi.com/Y8Y7H8BNJ"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img
-                  height="36"
-                  style={{ border: 0, height: 36 }}
-                  src="https://storage.ko-fi.com/cdn/kofi3.png?v=3"
-                  alt="Buy Me a Coffee at ko-fi.com"
-                />
-              </a>
             </FooterContainer>
           </Row>
         </Container>
